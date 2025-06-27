0aec964a06a6966cfed9bbee52fa69919c8f6d86
gnuleospython <ambiental2024b@netcourrier.com>
2025-06-27 01:00:11 -0500

    actualiz py

--- clase2/leo.py
+++ clase2/leo.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-#  graficarXYcolor.py
+#  tarea2.py
 #  
-#  Copyright 2025 leontv <leontv@svdorleosmath>
+#  Copyright 2025 leontv <leontv@svdor.net>
